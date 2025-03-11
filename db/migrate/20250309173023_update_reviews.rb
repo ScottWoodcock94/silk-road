841b93832b9d20d2655791ce4caa005b3d02300b
ScottWoodcock94 <scottwoodcock94.official@gmail.com>
2025-03-11 14:09:13 +0000

    Removed duplicate foreign key constraint. Added 'author_id' foreign key constraint.

--- db/migrate/20250309173023_update_reviews.rb
+++ db/migrate/20250309173023_update_reviews.rb
@@ -1,6 +1,6 @@
 class UpdateReviews < ActiveRecord::Migration[8.0]
   def change
     add_foreign_key :reviews, :users
-    add_foreign_key :reviews, :users
+    add_foreign_key :reviews, :users, column: :author_id
   end
 end
