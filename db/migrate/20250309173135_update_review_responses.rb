41e2f5df0ceb2a002760a6ecf786d104ec9b2dc3
ScottWoodcock94 <scottwoodcock94.official@gmail.com>
2025-03-11 16:53:09 +0000

    Adjusted foreign keys.

--- db/migrate/20250309173135_update_review_responses.rb
+++ db/migrate/20250309173135_update_review_responses.rb
@@ -1,5 +1,6 @@
 class UpdateReviewResponses < ActiveRecord::Migration[8.0]
   def change
     add_foreign_key :review_responses, :reviews
+    add_foreign_key :review_responses, :users, column: :author_id
   end
 end
